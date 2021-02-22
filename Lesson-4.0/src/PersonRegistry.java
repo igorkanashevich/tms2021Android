41509bc0c971127299001dfa60a3417b2ab85207
igorkanashevich <kanashevichigor1998@gmail.com>
2021-02-22 19:10:27 +0300

    HomeWork 5.1

--- Lesson-4.0/src/PersonRegistry.java
+++ Lesson-4.0/src/PersonRegistry.java
@@ -13,28 +13,28 @@ public class PersonRegistry {
         this.citizens = citizens;
     }
 
-//    public int countPeople(Address address) {
-//        int count = 0;
-//        for (Person person : citizens) {
-//            Address personsAddress = person.getAddress();
-//            if (personsAddress.getCountry().equals(address.getCountry())
-//                    && personsAddress.getCity().equals(address.getCity())) {
-//                count++;
-//            }
-//        }
-//        return count;
-//    }
-//
-//    public int countPeople(String country) {
-//        int count = 0;
-//        for (Person person : citizens) {
-//            Address personsAddress = person.getAddress();
-//            if (personsAddress.getCountry().equals(country)) {
-//                count++;
-//            }
-//        }
-//        return count;
-//    }
+    public int countPeople(Address address) {
+        int count = 0;
+        for (Person person : citizens) {
+            Address personsAddress = person.getAddress();
+            if (personsAddress.getCountry().equals(address.getCountry())
+                    && personsAddress.getCity().equals(address.getCity())) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int countPeople(String country) {
+        int count = 0;
+        for (Person person : citizens) {
+            Address personsAddress = person.getAddress();
+            if (personsAddress.getCountry().equals(country)) {
+                count++;
+            }
+        }
+        return count;
+    }
 
     public List<Person> getPeople(Address address) {
         List<Person> people = new LinkedList<>();
